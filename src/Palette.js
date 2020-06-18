38ff47cc0f172a4249bbc786406da0660b82985c
duhbhavesh <connect.bhavesh.kasturi@gmail.com>
2020-06-18 16:59:22 +0530

    initial commit

---
+++ src/Palette.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import ColorBox from "./ColorBox";
+import "./Palette.css";
+
+class Palette extends Component {
+	render() {
+		const colorBoxes = this.props.colors.map((color) => (
+			<ColorBox background={color.color} name={color.name} />
+		));
+		return (
+			<div className="Palette">
+				{/* Navbar goes here */}
+				<div className="Palette-colors">{colorBoxes}</div>
+				{/* Fotter goes her */}
+			</div>
+		);
+	}
+}
+
+export default Palette;
