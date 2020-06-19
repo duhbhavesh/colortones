15eb6869ee676deb4099141bb2a8496af3ff9244
duhbhavesh <connect.bhavesh.kasturi@gmail.com>
2020-06-19 14:23:20 +0530

    Add Navbar, Slider, CSS

--- src/Palette.js
+++ src/Palette.js
@@ -1,20 +1,31 @@
 import React, { Component } from "react";
 import ColorBox from "./ColorBox";
+import Navbar from "./Navbar";
 import "./Palette.css";
 
 class Palette extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { level: 500 };
+		this.changeLevel = this.changeLevel.bind(this);
+	}
+	changeLevel(level) {
+		this.setState({ level });
+	}
 	render() {
-		const colorBoxes = this.props.colors.map((color) => (
-			<ColorBox background={color.color} name={color.name} />
+		const { colors } = this.props.palette;
+		const { level } = this.state;
+		const colorBoxes = colors[level].map((color) => (
+			<ColorBox background={color.hex} name={color.name} />
 		));
 		return (
 			<div className="Palette">
+				<Navbar level={level} changeLevel={this.changeLevel} />
 				{/* Navbar goes here */}
 				<div className="Palette-colors">{colorBoxes}</div>
-				{/* Fotter goes her */}
+				{/* footer eventually */}
 			</div>
 		);
 	}
 }
-
 export default Palette;
