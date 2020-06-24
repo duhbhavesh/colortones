1d8ac2d25d5910d013d760b5d24858dd9e3a5eb6
duhbhavesh <connect.bhavesh.kasturi@gmail.com>
2020-06-24 20:22:07 +0530

    Refactoring and Adding drag and drop

--- src/App.js
+++ src/App.js
@@ -8,16 +8,34 @@ import NewPaletteForm from "./NewPaletteForm";
 import { generatePalette } from "./colorHelpers";
 
 class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { palettes: seedColors };
+		this.savePalette = this.savePalette.bind(this);
+		this.findPalette = this.findPalette.bind(this);
+	}
 	findPalette(id) {
-		return seedColors.find(function (palette) {
+		return this.state.palettes.find(function (palette) {
 			return palette.id === id;
 		});
 	}
-
+	savePalette(newPalette) {
+		this.setState({ palettes: [...this.state.palettes, newPalette] });
+	}
 	render() {
 		return (
 			<Switch>
-				<Route exact path="/palette/new" render={() => <NewPaletteForm />} />
+				<Route
+					exact
+					path="/palette/new"
+					render={(routeProps) => (
+						<NewPaletteForm
+							savePalette={this.savePalette}
+							palettes={this.state.palettes}
+							{...routeProps}
+						/>
+					)}
+				/>
 				<Route
 					exact
 					path="/palette/:paletteId/:colorId"
@@ -34,7 +52,7 @@ class App extends Component {
 					exact
 					path="/"
 					render={(routeProps) => (
-						<PaletteList palettes={seedColors} {...routeProps} />
+						<PaletteList palettes={this.state.palettes} {...routeProps} />
 					)}
 				/>
 				<Route
@@ -49,10 +67,6 @@ class App extends Component {
 					)}
 				/>
 			</Switch>
-
-			// <div>
-			// 	<Palette palette={generatePalette(seedColors[4])} />
-			// </div>
 		);
 	}
 }
--- src/DraggableColorBox.js
+++ src/DraggableColorBox.js
@@ -1,5 +1,7 @@
 import React from "react";
+import { SortableElement } from "react-sortable-hoc";
 import { withStyles } from "@material-ui/styles";
+import DeleteIcon from "@material-ui/icons/Delete";
 
 const styles = {
 	root: {
@@ -10,17 +12,39 @@ const styles = {
 		position: "relative",
 		cursor: "pointer",
 		marginBottom: "-3.5px",
+		"&:hover svg": {
+			color: "white",
+			transform: "scale(1.3)",
+		},
+	},
+	boxContent: {
+		position: "absolute",
+		width: "100%",
+		left: "0px",
+		bottom: "0px",
+		padding: "10px",
+		color: "rgba(0, 0, 0, 0.5)",
+		letterSpacing: "1px",
+		textTransform: "uppercase",
+		fontSize: "12px",
+		display: "flex",
+		justifyContent: "space-between",
+	},
+	deleteIcon: {
+		transition: "all 0.3s ease-in-out",
 	},
 };
 
-function DraggableColorBox(props) {
+const DraggableColorBox = SortableElement((props) => {
+	const { classes, handleClick, name, color } = props;
+
 	return (
-		<div
-			className={props.classes.root}
-			style={{ backgroundColor: props.color }}
-		>
-			{props.name}
+		<div className={classes.root} style={{ backgroundColor: color }}>
+			<div className={classes.boxContent}>
+				<span> {name}</span>
+				<DeleteIcon className={classes.deleteIcon} onClick={handleClick} />
+			</div>
 		</div>
 	);
-}
+});
 export default withStyles(styles)(DraggableColorBox);
