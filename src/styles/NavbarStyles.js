ecae75080e1a39c5b76987d2c87fa668204200b8
duhbhavesh <connect.bhavesh.kasturi@gmail.com>
2020-06-25 11:56:48 +0530

    Make create form responsive

--- src/styles/NavbarStyles.js
+++ src/styles/NavbarStyles.js
@@ -1,9 +1,17 @@
+import sizes from "./sizes";
+
 export default {
 	Navbar: {
 		display: "flex",
 		alignItems: "center",
 		justifyContent: "flex-start",
-		height: "7vh",
+		height: "6vh",
+		[sizes.down("xs")]: {
+			flexDirection: "column",
+			height: "auto",
+			justifyContent: "center",
+			padding: "1rem 0",
+		},
 	},
 	logo: {
 		marginRight: "15px",
@@ -14,13 +22,18 @@ export default {
 		height: "100%",
 		display: "flex",
 		alignItems: "center",
+		[sizes.down("xs")]: {
+			padding: "0.5rem",
+			marginBottom: "1rem",
+			borderRadius: "1.5rem",
+		},
 		"& a": {
 			textDecoration: "none",
 			color: "black",
 		},
 	},
 	slider: {
-		width: "200px",
+		width: "340px",
 		margin: "0 10px",
 		display: "inline-block",
 		"& .rc-slider-track": {
@@ -30,17 +43,25 @@ export default {
 			height: "8px",
 		},
 		"& .rc-slider-handle, .rc-slider-handle:active, .rc-slider-handle:focus,.rc-slider-handle:hover": {
-			backgroundColor: "cyan",
+			backgroundColor: "green",
 			outline: "none",
-			border: "1px solid #4c5561",
+			border: "2px solid green",
 			boxShadow: "none",
 			width: "13px",
 			height: "13px",
+			marginLeft: "-7px",
 			marginTop: "-3px",
+		},
+		[sizes.down("md")]: {
+			width: "150px",
 		},
 	},
 	selectContainer: {
 		marginLeft: "auto",
 		marginRight: "1rem",
+		[sizes.down("xs")]: {
+			marginLeft: "0",
+			marginTop: "1rem",
+		},
 	},
 };
--- src/styles/PaletteFormNavStyles.js
+++ src/styles/PaletteFormNavStyles.js
@@ -1,4 +1,6 @@
 import { DRAWER_WIDTH } from "../constants";
+import sizes from "./sizes";
+
 const drawerWidth = DRAWER_WIDTH;
 const styles = (theme) => ({
 	root: {
@@ -34,9 +36,16 @@ const styles = (theme) => ({
 		"& a": {
 			textDecoration: "none",
 		},
+		[sizes.down("xs")]: {
+			marginRight: "0.5rem",
+		},
 	},
 	button: {
 		margin: "0 0.5rem",
+		[sizes.down("xs")]: {
+			margin: "0 0.2rem",
+			padding: "0.3rem",
+		},
 	},
 });
 
